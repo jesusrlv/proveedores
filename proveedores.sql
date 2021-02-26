ffc441a85f0faeeddad9c24c02ac7520cb87632c
Mi nombreRodolfo <jesusrlv.informatica@gmail.com>
2021-02-26 15:40:26 -0600

    vistas terminadas

--- proveedores.sql
+++ proveedores.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost
--- Tiempo de generación: 22-02-2021 a las 23:26:51
+-- Tiempo de generación: 26-02-2021 a las 22:29:44
 -- Versión del servidor: 10.1.37-MariaDB
 -- Versión de PHP: 7.3.1
 
@@ -25,6 +25,75 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `datos`
+--
+
+CREATE TABLE `datos` (
+  `id` int(11) NOT NULL,
+  `id_ext` int(11) NOT NULL,
+  `fisc_apellido` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `fisc_nombre` varchar(70) COLLATE utf8_unicode_ci NOT NULL,
+  `fisc_curp` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `rfc` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `moral_razon` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `moral_denominacion` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `moral_archivo_acta` varchar(300) COLLATE utf8_unicode_ci NOT NULL,
+  `tipo_usr` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `direccion`
+--
+
+CREATE TABLE `direccion` (
+  `id` int(11) NOT NULL,
+  `id_ext` int(11) NOT NULL,
+  `colonia` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `cp` int(11) NOT NULL,
+  `n_int` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
+  `n_ext` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  `tel_of` int(11) NOT NULL,
+  `tel_cel` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `documentos`
+--
+
+CREATE TABLE `documentos` (
+  `id` int(11) NOT NULL,
+  `id_ext` int(11) NOT NULL,
+  `rcb1` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `cedula_prov` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `const_fiscal` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `comp_domicilio` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `caratula_edo_cuenta` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `ine` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `formato_d32` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `opinion_estatal` varchar(200) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `menu`
+--
+
+CREATE TABLE `menu` (
+  `id` int(11) NOT NULL,
+  `menu` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  `logo` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `a_h_ref` varchar(300) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `usr`
 --
 
@@ -40,6 +109,30 @@ CREATE TABLE `usr` (
 --
 
 --
+-- Indices de la tabla `datos`
+--
+ALTER TABLE `datos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `direccion`
+--
+ALTER TABLE `direccion`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `documentos`
+--
+ALTER TABLE `documentos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `menu`
+--
+ALTER TABLE `menu`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indices de la tabla `usr`
 --
 ALTER TABLE `usr`
@@ -48,6 +141,30 @@ ALTER TABLE `usr`
 --
 -- AUTO_INCREMENT de las tablas volcadas
 --
+
+--
+-- AUTO_INCREMENT de la tabla `datos`
+--
+ALTER TABLE `datos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `direccion`
+--
+ALTER TABLE `direccion`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `documentos`
+--
+ALTER TABLE `documentos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `menu`
+--
+ALTER TABLE `menu`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `usr`
